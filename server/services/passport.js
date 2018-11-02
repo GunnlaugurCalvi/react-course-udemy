259c2493f923c6dccfe90337381d74481a6200b2
Gunnlaugur Kristinn Hreidarsson Calvi <gunnlaugur1995@gmail.com>
2018-11-02 00:54:26 +0000

    Saving user to mongoDB after signup

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys  = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
     new GoogleStrategy({
@@ -9,8 +12,21 @@ passport.use(
         callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken', accessToken);
-        console.log('refreshToken', refreshToken);
-        console.log('profile:', profile);
+        User.findOne({ googleID: profile.id })
+            .then( existingUser => {
+                if (existingUser)
+                {
+                    //already have a record with the given googleID
+                    done(null, existingUser);
+                }
+                else
+                {
+                    //we dont have a user record with this google ID
+                    new User({
+                        googleID: profile.id,
+                        name: profile.displayName
+                    }).save().then( user => done(null, user));
+                }
+            });
     })
 );
